ed6d2547c787c170fc19d22d0e9c6149cae5d788
Sarah <sarahejohnson89@gmail.com>
2021-08-01 14:37:52 -0700

    Use to return a Boolean value representing whether or not a number is even

---
+++ even.js
@@ -0,0 +1,23 @@
+const isEven = function (num) {
+  return num % 2 === 0;
+}
+
+const isEven = function (num) {
+  return num % 2 === 0;
+}
+
+
+
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
+
+//This will produce the same result as above
+const isEven = function (num) {
+  return num % 2 === 0;
+}
+
+console.log(isEven(10));
+console.log(isEven(11));
