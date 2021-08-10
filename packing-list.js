f4017957071640fff84935abed055a49dae96119
Sarah <sarahejohnson89@gmail.com>
2021-08-10 09:11:50 -0700

    Use for a while loop and for.. loop with an array

---
+++ packing-list.js
@@ -0,0 +1,18 @@
+
+
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+//for loop with array
+
+// console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
+
+// a while loop with array
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
